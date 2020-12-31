ab59655001a229b256066eb4210947cfe0d7e879
SirLire <62445994+SirLire@users.noreply.github.com>
2020-12-31 12:56:31 +0100

    Versione iniziale progetto

---
+++ prog_vr/MuseHome/Assets/Scripts/FPSController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : PortalTraveller {
+
+    public bool lockCursor;
+
+    CharacterController controller;
+    Camera cam;
+    public float yaw;
+    public float pitch;
+    float smoothYaw;
+    float smoothPitch;
+    
+    Vector3 velocity;
+
+    bool jumping;
+    float lastGroundedTime;
+    bool disabled;
+
+    void Start () {
+        cam = Camera.main;
+        if (lockCursor) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        controller = GetComponent<CharacterController> ();
+
+        yaw = transform.eulerAngles.y;
+        pitch = cam.transform.localEulerAngles.x;
+        smoothYaw = yaw;
+        smoothPitch = pitch;
+    }
+
+    public override void Teleport (Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot) {
+        transform.position = pos;
+        Vector3 eulerRot = rot.eulerAngles;
+        float delta = Mathf.DeltaAngle (smoothYaw, eulerRot.y);
+        yaw += delta;
+        smoothYaw += delta;
+        transform.eulerAngles = Vector3.up * smoothYaw;
+        velocity = toPortal.TransformVector (fromPortal.InverseTransformVector (velocity));
+        Physics.SyncTransforms ();
+    }
+
+}
---
+++ prog_vr/MuseHome/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [Header("Main Settings")]
+    public Teleporter linkedPortal_next;
+    public Teleporter linkedPortal_previous;
+
+    [Header("Advanced Settings")]
+
+    // Private variables
+    List<PortalTraveller> trackedTravellers;
+
+    void Awake()
+    {
+        //playerCam = Camera.main;
+        trackedTravellers = new List<PortalTraveller>();
+    }
+
+    void LateUpdate()
+    {
+        HandleTravellers();
+    }
+
+    void HandleTravellers()
+    {
+
+        for (int i = 0; i < trackedTravellers.Count; i++)
+        {
+            PortalTraveller traveller = trackedTravellers[i];
+            Transform travellerT = traveller.transform;
+
+            Vector3 offsetFromPortal = travellerT.position - transform.position;
+            int portalSide = System.Math.Sign(Vector3.Dot(offsetFromPortal, transform.forward));
+            int portalSideOld = System.Math.Sign(Vector3.Dot(traveller.previousOffsetFromPortal, transform.forward));
+            // Teleport the traveller if it has crossed from one side of the portal to the other
+            if (portalSide != portalSideOld)
+            {
+                var linkedPortal = linkedPortal_next;
+                if (portalSide > 0)
+                {
+                    linkedPortal = linkedPortal_previous;
+                }
+                var m = linkedPortal.transform.localToWorldMatrix * transform.worldToLocalMatrix * travellerT.localToWorldMatrix;
+                var positionOld = travellerT.position;
+                var rotOld = travellerT.rotation;
+                traveller.Teleport(transform, linkedPortal.transform, m.GetColumn(3), m.rotation);
+                // Can't rely on OnTriggerEnter/Exit to be called next frame since it depends on when FixedUpdate runs
+                linkedPortal.OnTravellerEnterPortal(traveller);
+                trackedTravellers.RemoveAt(i);
+                i--;
+
+            }
+            else
+            {
+                //UpdateSliceParams (traveller);
+                traveller.previousOffsetFromPortal = offsetFromPortal;
+            }
+        }
+    }
+
+    void OnTravellerEnterPortal(PortalTraveller traveller)
+    {
+        if (!trackedTravellers.Contains(traveller))
+        {
+            traveller.EnterPortalThreshold();
+            traveller.previousOffsetFromPortal = traveller.transform.position - transform.position;
+            trackedTravellers.Add(traveller);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var traveller = other.GetComponent<PortalTraveller>();
+        if (traveller)
+        {
+            OnTravellerEnterPortal(traveller);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        var traveller = other.GetComponent<PortalTraveller>();
+        if (traveller && trackedTravellers.Contains(traveller))
+        {
+            traveller.ExitPortalThreshold();
+            trackedTravellers.Remove(traveller);
+        }
+    }
+
+    int SideOfPortal(Vector3 pos)
+    {
+        return System.Math.Sign(Vector3.Dot(pos - transform.position, transform.forward));
+    }
+
+    bool SameSideOfPortal(Vector3 posA, Vector3 posB)
+    {
+        return SideOfPortal(posA) == SideOfPortal(posB);
+    }
+
+    void OnValidate()
+    {
+        if (linkedPortal_next != null)
+        {
+            linkedPortal_next.linkedPortal_previous = this;
+        }
+        if (linkedPortal_previous != null)
+        {
+            linkedPortal_previous.linkedPortal_next = this;
+        }
+    }
+}
