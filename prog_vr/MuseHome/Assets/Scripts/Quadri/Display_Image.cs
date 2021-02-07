bef40ebdeff347b7f991083365c25992077ea48f
SirLire <62445994+SirLire@users.noreply.github.com>
2021-02-07 23:18:12 +0100

    Caricamento quadri

---
+++ prog_vr/MuseHome/Assets/Scripts/Quadri/Display_Image.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class Display_Image : MonoBehaviour
+{
+    public MeshRenderer plane;
+    public float minSize = (float)0.3;
+    public float maxSize = (float)2.0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        InstantiateImage("", Random.Range(minSize, maxSize));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InstantiateImage(string img, float img_width)
+    {
+        if (img.Length == 0)
+        {
+            var files = Directory.GetFiles(Application.streamingAssetsPath, "*.jpg", SearchOption.AllDirectories);
+            img = files[Random.Range(0, files.Length)];
+        }
+
+        byte[] bytes = File.ReadAllBytes(img);
+        Texture2D texture = new Texture2D(2, 2);
+        texture.filterMode = FilterMode.Trilinear;
+        texture.LoadImage(bytes);
+
+        float w = texture.width;
+        float h = texture.height;
+        float img_height = img_width * (h / w);
+
+        this.gameObject.GetComponentInChildren<Renderer>().material.mainTexture = texture;
+        this.gameObject.transform.localScale = new Vector3(img_width, img_height, 1);
+    }
+}
---
+++ prog_vr/MuseHome/Assets/Scripts/Quadri/Image_Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Image_Spawner : MonoBehaviour
+{
+    public int N_Targets = 5;
+    public GameObject Target;
+    
+    
+    void Awake()
+    {
+        for (int i = 0; i < N_Targets; i++)
+        {
+            Vector3 location = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), Random.Range(-1.0f, 1.0f));
+            GameObject new_Target = Instantiate(Target, location, Quaternion.identity);
+        }
+    }
+}
