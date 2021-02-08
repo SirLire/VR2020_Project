e647014c170807d2279aca624c69b0b9bcd68024
SirLire <62445994+SirLire@users.noreply.github.com>
2021-02-08 21:15:03 +0100

    Instanziamento cornici

--- prog_vr/MuseHome/Assets/Scripts/Quadri/Display_Image.cs
+++ prog_vr/MuseHome/Assets/Scripts/Quadri/Display_Image.cs
@@ -4,26 +4,40 @@ using System.IO;
 public class Display_Image : MonoBehaviour
 {
     public MeshRenderer plane;
-    public float minSize = (float)0.3;
-    public float maxSize = (float)2.0;
-    // Start is called before the first frame update
+    public float minSize = 0.3f;
+    public float maxSize = 2.0f;
+    public float maxHeight = 3.0f;
+    public GameObject latoQuadro;
+    public GameObject angoloQuadro;
+    public float spessoreCornice = 0.05f;
+
+    private System.Collections.Generic.List<GameObject> _cornice = new System.Collections.Generic.List<GameObject>();
+
+
     void Awake()
     {
-        InstantiateImage("", Random.Range(minSize, maxSize));
+        InstantiateImage("", 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        foreach(GameObject g in _cornice)
+        {
+            Destroy(g);
+        }
     }
 
-    void InstantiateImage(string img, float img_width)
+    float InstantiateImage(string img, float img_width)
     {
+        float img_true_width = 0;
         if (img.Length == 0)
         {
             var files = Directory.GetFiles(Application.streamingAssetsPath, "*.jpg", SearchOption.AllDirectories);
             img = files[Random.Range(0, files.Length)];
+        }
+        if (img_width == 0)
+        {
+            img_true_width = Random.Range(minSize, maxSize);
         }
 
         byte[] bytes = File.ReadAllBytes(img);
@@ -33,9 +47,55 @@ public class Display_Image : MonoBehaviour
 
         float w = texture.width;
         float h = texture.height;
-        float img_height = img_width * (h / w);
+        float img_height = img_true_width * (h / w);
+
+        if (img_height > maxHeight)
+        {
+            img_height = maxHeight;
+            img_true_width = img_height * (w / h);
+        }
 
         this.gameObject.GetComponentInChildren<Renderer>().material.mainTexture = texture;
-        this.gameObject.transform.localScale = new Vector3(img_width, img_height, 1);
+        this.gameObject.transform.localScale = new Vector3(img_true_width, img_height, 1);
+
+        AggiungiCornice(img_true_width, img_height);
+
+        return img_true_width;
+    }
+
+    void AggiungiCornice (float width, float height)
+    {
+        GameObject new_Target;
+        Vector3 posizioneQuadro = this.gameObject.transform.position;
+        
+        //lati sinistro e destro
+        new_Target = Instantiate(latoQuadro, new Vector3(posizioneQuadro.x - (width + spessoreCornice) / 2, posizioneQuadro.y, posizioneQuadro.z), Quaternion.identity);
+        new_Target.transform.localScale = new Vector3(1, height, 1);
+        _cornice.Add(new_Target);
+        new_Target = Instantiate(latoQuadro, new Vector3(posizioneQuadro.x + (width + spessoreCornice) / 2, posizioneQuadro.y, posizioneQuadro.z), Quaternion.identity);
+        new_Target.transform.localScale = new Vector3(1, height, 1);
+        _cornice.Add(new_Target);
+        //lati superiore e inferiore 
+        new_Target = Instantiate(latoQuadro, new Vector3(posizioneQuadro.x, posizioneQuadro.y - (height + spessoreCornice) / 2, posizioneQuadro.z), Quaternion.identity);
+        new_Target.transform.localScale = new Vector3(1, width, 1);
+        new_Target.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
+        _cornice.Add(new_Target);
+        new_Target = Instantiate(latoQuadro, new Vector3(posizioneQuadro.x, posizioneQuadro.y + (height + spessoreCornice) / 2, posizioneQuadro.z), Quaternion.identity);
+        new_Target.transform.localScale = new Vector3(1, width, 1);
+        new_Target.transform.Rotate(0.0f, 0.0f, -90.0f, Space.Self);
+        _cornice.Add(new_Target);
+
+        //angoli
+        new_Target = Instantiate(angoloQuadro, new Vector3(posizioneQuadro.x - (width + spessoreCornice) / 2, posizioneQuadro.y + (height + spessoreCornice) / 2, posizioneQuadro.z), Quaternion.identity);
+        _cornice.Add(new_Target);
+        new_Target = Instantiate(angoloQuadro, new Vector3(posizioneQuadro.x + (width + spessoreCornice) / 2, posizioneQuadro.y + (height + spessoreCornice) / 2, posizioneQuadro.z), Quaternion.identity);
+        new_Target.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
+        _cornice.Add(new_Target);
+        new_Target = Instantiate(angoloQuadro, new Vector3(posizioneQuadro.x - (width + spessoreCornice) / 2, posizioneQuadro.y - (height + spessoreCornice) / 2, posizioneQuadro.z), Quaternion.identity);
+        new_Target.transform.Rotate(0.0f, 0.0f, 180.0f, Space.Self);
+        _cornice.Add(new_Target);
+        new_Target = Instantiate(angoloQuadro, new Vector3(posizioneQuadro.x + (width + spessoreCornice) / 2, posizioneQuadro.y - (height + spessoreCornice) / 2, posizioneQuadro.z), Quaternion.identity);
+        new_Target.transform.Rotate(0.0f, 0.0f, 270.0f, Space.Self);
+        _cornice.Add(new_Target);
     }
 }
