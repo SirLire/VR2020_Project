46dea3c69d96c39dd0e66d48602abf1f3cd34a2b
LorenzoCannillo <lorenzo.cannillo@gmail.com>
2021-02-17 19:43:04 +0100

    - preLoad Portal Scene

--- prog_vr/MuseHome/Assets/Scripts/Main_Menu.cs
+++ prog_vr/MuseHome/Assets/Scripts/Main_Menu.cs
@@ -8,23 +8,48 @@ public class Main_Menu : MonoBehaviour
     public Animator transition;
     public float transitionTime = 2f;
     public bool inputB = false;
+    private bool singleton = false;
+    private bool single_load = false;
+    private AsyncOperation _asyncLoad;
 
-   void Update()
+    private void Start()
     {
+/*
+        _asyncLoad = SceneManager.LoadSceneAsync("PortalScene");
+        _asyncLoad.allowSceneActivation = false;
+*/
+    }
+    void Update()
+    {
+        if (!single_load)
+        {
+            StartCoroutine(PreLoad());
+            single_load = true;
+        }
         if (OVRInput.GetDown(OVRInput.RawButton.B))
             inputB = !inputB;
-        if (inputB==true && SceneManager.GetActiveScene().name == "Main_Menu")
+        if (inputB==true && SceneManager.GetActiveScene().name == "Main_Menu" && singleton==false)
         {
             StartCoroutine(LoadLevel("PortalScene"));
+            singleton = true;
         }
         
     }
+    IEnumerator PreLoad()
+    {
+        yield return new WaitForSeconds(0.1f);
+        _asyncLoad = SceneManager.LoadSceneAsync("PortalScene");
+        _asyncLoad.allowSceneActivation = false;
+        yield return new WaitForSeconds(0);
+    }
     IEnumerator LoadLevel(string levelIndex)
     {
+        Debug.Log("SONO QUI AIUTO");
         transition.SetTrigger("Start");
 
-        yield return new WaitForSeconds(transitionTime);
-
-        SceneManager.LoadScene(levelIndex);
+        //yield return new WaitForSeconds(transitionTime);
+        yield return new WaitUntil(() => _asyncLoad.progress >=0.9f);
+        _asyncLoad.allowSceneActivation = true;
+        //SceneManager.LoadScene(levelIndex);
     }
 }
