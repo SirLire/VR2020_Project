2fbfaffd9e604fdd8afe9ae0ca88263b07ac075f
LAPTOP-6QAB9QAC\danny <dannydav@hotmail.it>
2021-02-17 17:33:44 +0100

    Added main menu

---
+++ prog_vr/MuseHome/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 2f;
+
+   void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.RawButton.B) && SceneManager.GetActiveScene().buildIndex==0)
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
